606973eeaa7fa9f222e04c9c7690d28e42107bf5
kmuylaert <kris.muylaert@ilvo.vlaanderen.be>
2026-08-19 09:51:22 +0200

    fix delete sample

--- SmartDots/Helpers/WebAPI.cs
+++ SmartDots/Helpers/WebAPI.cs
@@ -335,7 +335,8 @@ namespace SmartDots.Helpers
 
         WebApiResult<bool> ISmartDotsAPI.DeleteSample(Guid id)
         {
-            return PerformPost<bool,Object>("deletesample?token=" + CurrentUser.Token, new  { ID = id });
+            //return PerformPost<bool,Object>("deletesample?token=" + CurrentUser.Token, new  { ID = id });
+            return PerformPost<bool,Object>("deletesample?token=" + CurrentUser.Token, id);
         }
 
         //WebApiResult<DtoSmartdotsSettings> ISmartDotsAPI.GetSettings() //server dependent
